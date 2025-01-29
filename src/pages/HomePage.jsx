f9dcd9b6d4603d2f162b5d105bd539fbe09dd6cd
micoforreal <mjjames006@gmail.com>
2025-01-29 22:16:33 +0100

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -44,31 +44,33 @@ export default function HomePage() {
 
   // Fetch user data on mount
   useEffect(() => {
-   
+    if(telegram?.initDataUnsafe?.user){
     fetchUserData();
+    } else{
+      console.log("You are currently not using telegram ")
+    }
   }, [telegram]);
 
 
 
   
 const fetchUserData = async () => {
-  if(telegram?.initDataUnsafe?.user){
+  // if(telegram?.initDataUnsafe?.user){
   try {
     const postData= {
       telegramId:telegram.initDataUnsafe.user.id
     }
 
       const response = await axios.post('https://af5e-102-91-103-230.ngrok-free.app/api/user/init',postData);
-      if (response.ok) {
-        const data = await response.json();
-        setUserData(data);
+      if  (response.status === 200) {
+        setUserData(response.data); 
       }
     } catch (error) {
       console.error('Could Not get User Data:', error);
     }
-  }else{
-    console.log("You are currently not using telegram ")
-  }
+  // }else{
+  //   console.log("You are currently not using telegram ")
+  // }
 };
 
 
