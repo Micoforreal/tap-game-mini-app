a203c1e9f9af8fe42311e21000e666c3aea86fe9
micoforreal <mjjames006@gmail.com>
2025-02-16 16:33:57 +0100

    update

--- src/pages/Meme.jsx
+++ src/pages/Meme.jsx
@@ -1,7 +1,22 @@
 
 import { Copy, Gamepad2, Heart, RotateCcw, Skull, Smile } from "lucide-react";
+import {  useState } from "react";
 
 export default function Meme() {
+  const [input,setInputs] = useState({userPrompt:''})
+  const handleChange = (e)=>{
+    setInputs((prev)=>({...prev, [e.target.name]:e.target.value}))
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+
+    
+
+    
+  }
+
+
   return (
     <section className="flex flex-col items-center justify-start w-full px-4 pt-5 pb-10">
       <p className="text-3xl font-bold leading-none text-white font-grotesk">
@@ -11,16 +26,37 @@ export default function Meme() {
         Enter a prompt and choose your preferred style to generate your
         mind-blowing memes
       </p>
-      <section className="w-full max-w-[382px] bg-[#D9D9D9]/10 border border-[#60A5FA]/30  p-4 rounded-3xl flex flex-col justify-center items-end gap-6 mt-12">
-        <p className="text-sm font-semibold font-jakarta text-[#E5E5E5]">
-          Realistic meme of a cute otter zebra mouse in a field at sunset, tall
-          grass, macro 35mm film
-        </p>
-        <div className="bg-[#FFFFFF]/40 border border-[#D9D9D9]/30 rounded-full py-1 px-2 flex justify-center items-center gap-2">
+      <form className="w-full max-w-[382px]" onSubmit={handleSubmit}>
+        
+      <section className="   rounded-3xl flex flex-col justify-cente items-end gap- mt-12">
+       
+        <input 
+        placeholder="Enter a prompt"
+        value={input.userPrompt}
+        name="userPrompt"
+        onChange={handleChange}
+        className="w-full
+        
+        py-20
+        
+        relative h-[8rem] 
+        max-w-[382px] bg-[#D9D9D9]/10
+        border border-[#60A5FA]/30  
+        px-4 pt-2 rounded-3xl 
+        
+        text-white
+        "/>
+
+        <div className="bg-[#FFFFFF]/40 border relative bottom-8 right-2 border-[#D9D9D9]/30 rounded-full py-0.5 px-2 flex justify-center items-center  gap-2">
           <Copy className="size-4 stroke-[#1A1A1A]" />
           <RotateCcw className="size-4 stroke-[#1A1A1A]" />
         </div>
+
+        <button className=" border-[#60A5FA]/30 bg-accent  rounded-full py-2 px-4">Generate</button>
       </section>
+
+        </form>
+
       <section className="flex flex-col items-start justify-start w-full gap-6 mt-7">
         <p className="text-sm font-jakarta font-medium text-[#E4DEEF]">
           Select a style
